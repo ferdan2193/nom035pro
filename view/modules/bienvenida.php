30bfa72bae1bb4d8af596c39a63b062601ea7e81
cierodev <67662218+cierodev@users.noreply.github.com>
2020-09-25 13:01:50 -0500

    2 BOTONES COLPASO BIENVENIDA
    Se agrega sección bienvenida con menu colapso

---
+++ view/modules/bienvenida.php
@@ -0,0 +1,92 @@
+<div class="section section0" id="bienvenida">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <p class="titulo-section">BIENVENIDA</p>
+        </div>
+    </div>
+    <div id="acordeon" class="row">
+        <div class="col-md-12">
+            <div id="descrbienvenida" class="text-center">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor. Nihil anim keffiyeh helvetica, craft
+                beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+            <input type="radio" id="animal" name="wiki" value="Animal" checked>
+            <input type="radio" id="plant" name="wiki" value="Plant">
+            <input type="radio" id="space" name="wiki" value="Space">
+            <input type="radio" id="river" name="wiki" value="River">
+
+            <ul class="accordion">
+                <li data-radio="animal">
+                    <label for="animal" class="accordion-title">
+                        <span>01</span>
+                        <span class="accordion-heading">Primera Sección</span>
+                    </label>
+                    <div class="accordion-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                        pulvinar dapibus est ac tempus. Donec dapibus finibus turpis id aliquet. Proin eu massa dui.
+                        Pellentesque volutpat scelerisque risus, a gravida tortor interdum non. Duis quis urna vehicula,
+                        scelerisque lectus ac, commodo libero. Nunc nec faucibus enim. Vivamus id est neque. Fusce
+                        fermentum
+                        et diam ut mattis. Aliquam varius eleifend volutpat. Suspendisse in bibendum purus. Proin
+                        condimentum interdum massa, eu egestas erat fringilla ac. Nam malesuada fermentum porta.</div>
+                </li>
+                <li data-radio="plant">
+                    <label for="plant" class="accordion-title">
+                        <span>02</span>
+                        <span class="accordion-heading">Segunda Sección</span>
+                    </label>
+                    <div class="accordion-content">
+
+                        Aenean ultrices est quis pulvinar bibendum. Vivamus et pellentesque velit. Nunc mollis sit amet
+                        nibh
+                        ac ullamcorper. Aliquam iaculis libero ut sem aliquam cursus. Fusce cursus ut felis non
+                        sollicitudin. Etiam magna orci, scelerisque at porta et, rhoncus quis enim. Etiam a convallis
+                        eros.
+                        Sed non ligula lacinia, interdum nulla et, bibendum velit. Integer velit risus, porta sed
+                        tincidunt
+                        ornare, pharetra euismod ipsum. Cras mollis odio vitae nibh malesuada bibendum non eget dui.
+                        Class
+                        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In
+                        placerat.</div>
+                </li>
+                <li data-radio="space">
+                    <label for="space" class="accordion-title">
+                        <span>03</span>
+                        <span class="accordion-heading">Tercera Sección</span>
+                    </label>
+                    <div class="accordion-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                        pulvinar dapibus est ac tempus. Donec dapibus finibus turpis id aliquet. Proin eu massa dui.
+                        Pellentesque volutpat scelerisque risus, a gravida tortor interdum non. Duis quis urna vehicula,
+                        scelerisque lectus ac, commodo libero. Nunc nec faucibus enim. Vivamus id est neque. Fusce
+                        fermentum
+                        et diam ut mattis. Aliquam varius eleifend volutpat. Suspendisse in bibendum purus. Proin
+                        condimentum interdum massa, eu egestas erat fringilla ac. Nam malesuada fermentum porta.</div>
+                </li>
+                <li data-radio="river">
+                    <label for="river" class="accordion-title">
+                        <span>04</span>
+                        <span class="accordion-heading">Cuarta sección</span>
+                    </label>
+                    <div class="accordion-content">Aenean ultrices est quis pulvinar bibendum. Vivamus et pellentesque
+                        velit. Nunc mollis sit amet nibh ac ullamcorper. Aliquam iaculis libero ut sem aliquam cursus.
+                        Fusce
+                        cursus ut felis non sollicitudin. Etiam magna orci, scelerisque at porta et, rhoncus quis enim.
+                        Etiam a convallis eros. Sed non ligula lacinia, interdum nulla et, bibendum velit. Integer velit
+                        risus, porta sed tincidunt ornare, pharetra euismod ipsum. Cras mollis odio vitae nibh malesuada
+                        bibendum non eget dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                        inceptos himenaeos. In placerat, leo eget tempus viverra, dui augue aliquam ante, congue
+                        facilisis
+                        sapien magna sit amet nulla. Vivamus enim risus, tincidunt vitae interdum feugiat, varius sit
+                        amet
+                        orci. Aenean nunc sapien, finibus sed pharetra eu, facilisis ac est. Phasellus facilisis quam et
+                        lacus consectetur, vitae aliquam quam dignissim. Maecenas non consequat risus.
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
