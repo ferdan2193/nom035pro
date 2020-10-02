0ca96f040d79ced8322ae099005e698d35a4e5a6
cierodev <67662218+cierodev@users.noreply.github.com>
2020-10-02 15:01:09 -0500

    5 APARTADO MEDIDAS
    Se agrega apartado medidas con las 3 secciones

---
+++ view/modules/medidas.php
@@ -0,0 +1,112 @@
+<div class="section section0" id="medidas">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <p class="titulo-section">MEDIDAS DE CONTROL</p>
+            <p class="subtitulo-section">PARA LOS RESULTADOS OBTENIDOS POR LA EVALUACIÓN</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 medidascontrol">
+            <p class="text-center" style="font-size:20px; font-family: 'Josefin Sans', sans-serif; color: #FFFFFF">
+                MEDIDAS DE CONTROL
+            </p>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> LIDERAZGO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> LIDERAZGO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> LIDERAZGO</p>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div clas="iconos">
+                        <button type="button" class="btn btn-primary btn-circle btn-xl">
+                            <i class="fas fa-bullhorn"></i>
+                        </button>
+                        <p class="iconos"> CARGAS DE TRABAJO</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 medidasfecha">
+            <div class="col-md-12">
+                <p>ELIGE UNA FECHA:
+                </p>
+                
+
+                <form>
+                    <input class="dateMedidas"type="date" id="birthday" name="birthday">
+                </form>
+           
+            </div>
+            <div class="col-md-12">
+
+            </div>
+
+        </div>
+        <div class="col-md-4 medidasavances">
+
+        </div>
+    </div>
+</div>
