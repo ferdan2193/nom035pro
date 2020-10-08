7a1d1ce5d59c11d6863df547126da4ba67079b61
cierodev <67662218+cierodev@users.noreply.github.com>
2020-10-08 11:20:28 -0500

    7 APARTADO BUZON SUGERENCIAS
    Sea agrega apartado de buzon

---
+++ view/modules/buzon.php
@@ -0,0 +1,84 @@
+<div class="container" id="buzon">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <p class="titulo-section">BUZÓN DE SUGERENCIAS</p>
+        </div>
+    </div>
+    <div class="row">
+        <div id="contbuzon">
+            <div id="descrbuzon" class="text-center">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
+                squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod. Brunch 3 wolf moon tempor. Nihil anim keffiyeh helvetica, craft
+                beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice
+                lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you
+                probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+        </div>
+
+    </div>
+    <div class="row cabecerabuzon">
+        <div class="col-md-6">
+            <p class="text-center"> Realiza tu comentario en este buzón: </p>
+        </div>
+        <div class="col-md-6">
+            <p class="text-center"> Mural de Registros </p>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <form>
+                <label>OBSERVACIONES</label> <br>
+                <textarea class="field" rows="4" name="observation"></textarea> <br>
+                <button class="field btnenviar" type="submit" id="send">Enviar</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <table class="table tbuzon">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-check text-center">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                    Acepto los términos y condiciones  <a scr="">CONFIDENCIALIDAD </a>
+                </label>
+            </div>
+
+        </div>
+        <div class="col-md-6">
+        </div>
+    </div>
+</div>
